088c5a94e4c8202db63b17ab67e229349975c1fd
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-01-18 17:34:07 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -0,0 +1,9 @@
+cro_alp = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+alp = input()
+cnt = 0
+cnt_alp = {}
+for i in cro_alp:
+    if i in alp:
+        cnt += 1
+        alp = alp.replace(i, '0')
+print(len(alp))
