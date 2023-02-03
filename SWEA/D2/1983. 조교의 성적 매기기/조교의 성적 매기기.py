62c541c1df5b3f25a5a34a45555ebdb377931739
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-02-03 13:51:09 +0900

    [D2] Title: 조교의 성적 매기기, Time: 139 ms, Memory: 52,392 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,22 +1,13 @@
 T = int(input())
-s_lst = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
-for i in range(1, T+1):
+grades = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0']
+for t in range(1, T+1):
     N, K = map(int, input().split())
-    stu_dict = {}
-    sort_dict = {}
     lst = []
-    r_lst = []
-    num_lst = list(range(N))
-    for j in range(1, N+1):
-        A, B, C = list(map(int, input().split()))
-        stu_dict[j] = (0.35*A)+(0.45*B)+(0.2*C)
-        lst.append((0.35*A)+(0.45*B)+(0.2*C))
-        r_lst = sorted(lst,reverse=True)
-    for k in range(len(s_lst)):
-        cnt = 0
-        for n in num_lst:
-            sort_dict[r_lst[num_lst.pop(0)]] = s_lst[k]
-            cnt += 1
-            if cnt == int(N/10):
-                break
-    print(f'#{i} {sort_dict[stu_dict[K]]}')  
+    for i in range(N):
+        student = list(map(int, input().split()))
+        lst.append(student[0]*0.35 + student[1]*0.45 + student[2]*0.2)
+    sorted_lst = sorted(lst, reverse=True)
+    dict_student = {}
+    for j in range(N):
+        dict_student[sorted_lst[j]] = grades[j//(N//10)]
+    print(f'#{t} {dict_student[lst[K-1]]}')
