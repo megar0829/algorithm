8315045fc85f03794643a94abb5f1be250136112
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-02-20 15:42:45 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 56 ms, Memory: 39848 KB -BaekjoonHub

--- 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.py
@@ -1,12 +1,13 @@
 import sys
+import random
 N = int(sys.stdin.readline())
 lst = []
-for i in range(N+1, 10**9):
+for i in range(N+1, 100*N):
     numbers = list(range(1, i+1))
     cnt = 0
     for j in numbers:
         if i % j == 0:
             cnt +=1
     if cnt > 2:
-        print(i)
-        break
+        lst.append(i)
+print(random.choice(lst))
