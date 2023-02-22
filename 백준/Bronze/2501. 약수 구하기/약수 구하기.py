1bd504e9c87ac1225d7c870f062dc6853cac47fe
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-02-22 16:20:09 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,9 @@
+N, K = list(map(int, input().split()))
+divisors = []
+for i in range(1, N+1):
+    if N % i == 0:
+        divisors.append(i)
+if len(divisors) < K:
+    print(0)
+else:
+    print(divisors[K-1])
