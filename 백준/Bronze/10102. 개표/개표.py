2c979bd4d1476a46f575d82bea2972920aedd17b
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-07-15 12:10:41 +0900

    [Bronze III] Title: 개표, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.py
@@ -0,0 +1,14 @@
+N = int(input())
+vote = input()
+a, b = 0, 0
+for i in vote:
+    if i == 'A':
+        a += 1
+    elif i == 'B':
+        b += 1
+if a > b:
+    print('A')
+elif a == b:
+    print('Tie')
+elif a < b:
+    print('B')
