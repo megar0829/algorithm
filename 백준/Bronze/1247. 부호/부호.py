dc020723698e06fda8ecea7198a382c8ae4f3d24
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-07-23 19:20:32 +0900

    [Bronze III] Title: 부호, Time: 200 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+for _ in range(3):
+    N = int(input())
+    S = 0
+    for i in range(N):
+        S += int(input())
+    if S > 0:
+        print('+')
+    elif S == 0:
+        print(0)
+    else:
+        print('-')
