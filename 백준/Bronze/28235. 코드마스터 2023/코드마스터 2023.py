72814fb847a9efdf028aafd721a0b26fe8edbd6a
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-07-29 22:58:47 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+text = input()
+
+if text == 'SONGDO':
+    print('HIGHSCHOOL')
+elif text == 'CODE':
+    print('MASTER')
+elif text == '2023':
+    print('0611')
+elif text == 'ALGORITHM':
+    print('CONTEST')
