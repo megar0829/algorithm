7863dba5df85f0135235447e9e9a9f08661d29ec
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-07-30 22:13:05 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+
+while True:
+    name, age, weight = list(map(str, input().split()))
+    if name == '#' and age == '0' and weight == '0':
+        break
+    if int(age) > 17 or int(weight) >= 80:
+        print(name, 'Senior')
+    else:
+        print(name, 'Junior')
