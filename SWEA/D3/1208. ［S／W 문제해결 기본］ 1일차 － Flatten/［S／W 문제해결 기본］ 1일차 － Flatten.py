860d3f40bd4c779831aece1c3607a4cadfe6352a
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-01 14:05:27 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 124 ms, Memory: 58,892 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.py
@@ -1,11 +1,23 @@
-for t in range(1, 11):
+# 카운팅 정렬
+def counting_sort(arr):
+    counts = [0] * 101
+    temp = [0] * 100
+    for i in arr:
+        counts[i] += 1
+    for i in range(1, 101):
+        counts[i] += counts[i - 1]
+    for i in range(99, -1, -1):
+        counts[arr[i]] -= 1
+        temp[counts[arr[i]]] = arr[i]
+    return temp
+
+for tc in range(1, 11):
     N = int(input())
-    boxes = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
+    arr = counting_sort(arr)
     for i in range(N):
-        if max(boxes) - min(boxes) <= 1:
-            break
-        else:
-            boxes[boxes.index(max(boxes))] -= 1
-            boxes[boxes.index(min(boxes))] += 1
-    print(f'#{t} {max(boxes) - min(boxes)}')
-            
+        arr[0] += 1
+        arr[-1] -= 1
+        arr = counting_sort(arr)
+    result = arr[-1] - arr[0]
+    print(f'#{tc} {result}')
