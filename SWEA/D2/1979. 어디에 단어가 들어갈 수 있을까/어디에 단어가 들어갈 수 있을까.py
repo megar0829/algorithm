1bb4de3fd37d0e8064454578bce571d6e0faba89
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-02 14:43:12 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 106 ms, Memory: 45,868 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,33 +1,34 @@
 T = int(input())
-for t in range(1, T+1):
+for tc in range(1, T + 1):
     N, K = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(N)]
-    cnt = 0
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    result = 0
     for i in range(N):
-        text_cnt = 0
+        row_lst = []
+        col_lst = []
+        cnt_row = 0
+        cnt_col = 0
         for j in range(N):
-            if matrix[i][j] == 0:
-                if text_cnt == K:
-                    cnt += 1
-                    text_cnt = 0
-                else:
-                    text_cnt = 0
-            else:
-                text_cnt += 1
-                if j == N-1 and text_cnt == K:
-                    cnt += 1
-    for i in range(N):
-        text_cnt = 0
-        for j in range(N):
-            if matrix[j][i] == 0:
-                if text_cnt == K:
-                    cnt += 1
-                    text_cnt = 0
-                else:
-                    text_cnt = 0
-            else:
-                text_cnt += 1
-                if j == N-1 and text_cnt == K:
-                    cnt += 1
-    
-    print(f'#{t} {cnt}')
+            # 행 우선 탐색
+            if arr[i][j] == 1:
+                cnt_row += 1
+                if j == N - 1:
+                    row_lst.append(cnt_row)
+            elif arr[i][j] == 0 and cnt_row != 0:
+                row_lst.append(cnt_row)
+                cnt_row = 0
+
+            # 열 우선 탐색
+            if arr[j][i] == 1:
+                cnt_col += 1
+                if j == N - 1:
+                    col_lst.append(cnt_col)
+            elif arr[j][i] == 0 and cnt_col != 0:
+                col_lst.append(cnt_col)
+                cnt_col = 0
+
+        if K in row_lst:
+            result += row_lst.count(K)
+        if K in col_lst:
+            result += col_lst.count(K)
+    print(f'#{tc} {result}')
