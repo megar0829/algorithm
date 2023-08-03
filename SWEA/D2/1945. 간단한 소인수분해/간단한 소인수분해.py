76f5b557f851d66ba0349d66d607ec771dd9d5e7
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-04 00:14:48 +0900

    [D2] Title: 간단한 소인수분해, Time: 95 ms, Memory: 43,772 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,16 +1,31 @@
+def factorization(n):
+    global a, b, c, d, e
+    if n == 1:
+        return a, b, c, d, e
+    elif n % 2 == 0:
+        n //= 2
+        a += 1 
+        return factorization(n)
+    elif n % 3 == 0:
+        n //= 3
+        b += 1
+        return factorization(n)
+    elif n % 5 == 0:
+        n //= 5
+        c += 1
+        return factorization(n)
+    elif n % 7 == 0:
+        n //= 7
+        d += 1
+        return factorization(n)
+    elif n % 11 == 0:
+        n //= 11
+        e += 1
+        return factorization(n)
+
+
 T = int(input())
-for t in range(1, T+1):
+for tc in range(1, T + 1):
     N = int(input())
-    abcde = [2, 3, 5, 7, 11]
-    numbers = [0, 0, 0, 0, 0]
-    i = 0
-    while True:
-        if N % abcde[i] == 0:
-            N = N / abcde[i]
-            numbers[i] += 1
-        i += 1
-        if i > 4:
-            i = 0
-        if N == 1:
-            break
-    print(f'#{t}', *numbers)
+    a, b, c, d, e = 0, 0, 0, 0, 0
+    print(f'#{tc}', *factorization(N))
