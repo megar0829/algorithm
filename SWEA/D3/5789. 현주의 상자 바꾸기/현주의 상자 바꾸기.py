58e3e6b804677390192bc63bb4144c7c6ea9583b
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-04 10:31:53 +0900

    [D3] Title: 현주의 상자 바꾸기, Time: 450 ms, Memory: 61,732 KB -BaekjoonHub

---
+++ SWEA/D3/5789. 현주의 상자 바꾸기/현주의 상자 바꾸기.py
@@ -0,0 +1,9 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N, Q = map(int, input().split())
+    arr = [0] * N
+    for i in range(1, Q + 1):
+        L, R = map(int, input().split())
+        for j in range(L - 1, R):
+            arr[j] = i
+    print(f'#{tc}', *arr)
