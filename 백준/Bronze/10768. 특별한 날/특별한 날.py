2433fe26c1eed5361d57fa360c32d8bc5f1db3a6
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-06 19:55:37 +0900

    [Bronze IV] Title: 특별한 날, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.py
@@ -0,0 +1,8 @@
+M = int(input())
+D = int(input())
+if M > 2 or (M == 2 and D > 18):
+    print('After')
+elif M < 2 or (M == 2 and D < 18):
+    print('Before')
+elif M == 2 and D == 18:
+    print('Special')
