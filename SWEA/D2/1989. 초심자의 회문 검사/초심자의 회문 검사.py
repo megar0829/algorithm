9f9c81e052de9de5ea113dcc015dedb81d3e52ef
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-07 16:05:00 +0900

    [D2] Title: 초심자의 회문 검사, Time: 101 ms, Memory: 43,776 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -1,7 +1,11 @@
+def palindrome(text):
+    n = len(text)
+    for i in range(n // 2):
+        if text[i] != text[n - 1 - i]:
+            return 0
+    return 1
+
+
 T = int(input())
-for i in range(1, T+1):
-    text = input()
-    if text == text[::-1]:
-        print(f'#{i} {1}')
-    else:
-        print(f'#{i} {0}')
+for tc in range(1, T + 1):
+    print(f'#{tc} {palindrome(input())}')
