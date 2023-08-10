906a1603874c34123fa51719e56203ab5a15a869
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-10 09:01:21 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -2,25 +2,28 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-lst_stack = []
+top = -1
+arr = [0] * N
 for _ in range(N):
-    order = list(map(str, input().split()))
-    if order[0] == 'push':
-        lst_stack.append(int(order[1]))
-    elif order[0] == 'pop':
-        if len(lst_stack) == 0:
+    text = input().split()
+    if text[0] == 'push':
+        top += 1
+        arr[top] = int(text[1])
+    elif text[0] == 'pop':
+        if top == -1:
             print(-1)
         else:
-            print(lst_stack.pop())
-    elif order[0] == 'size':
-        print(len(lst_stack))
-    elif order[0] == 'empty':
-        if len(lst_stack) == 0:
+            print(arr[top])
+            top -= 1
+    elif text[0] == 'size':
+        print(top + 1)
+    elif text[0] == 'empty':
+        if top == -1:
             print(1)
         else:
             print(0)
-    elif order[0] == 'top':
-        if len(lst_stack) == 0:
+    elif text[0] == 'top':
+        if top == -1:
             print(-1)
         else:
-            print(lst_stack[-1])
+            print(arr[top])
