9724fedf1615092a327f4ad69656a45b855b70bf
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-11 09:00:11 +0900

    [Silver V] Title: 피보나치 수 4, Time: 48 ms, Memory: 34916 KB -BaekjoonHub

--- 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -1,5 +1,7 @@
-N = int(input())
-arr = [0, 1] + [0] * (N - 1)
-for i in range(2, N + 1):
+# DP
+arr = [0, 1] + [0] * (9999)
+for i in range(2, 10001):
     arr[i] = arr[i - 1] + arr[i - 2]
+    
+N = int(input())
 print(arr[N])
