7447e7e43a2f6476a189b5ecd723d00a83f36e7b
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-12 23:08:10 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,15 +1,15 @@
 import sys
-T = int(sys.stdin.readline())
-floor_1 = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]]
+input = sys.stdin.readline
+
+arr = [[0] * 15 for _ in range(15)]
+for i in range(1, 15):
+    arr[0][i] = i
+for i in range(1, 15):
+    for j in range(1, 15):
+        for k in range(1, j + 1):
+            arr[i][j] += arr[i - 1][k]
+T = int(input())
 for _ in range(T):
-    k = int(sys.stdin.readline())
-    n = int(sys.stdin.readline())
-    for _ in range(k):
-        floor_1.append([0]*14)
-    for i in range(k):
-        for j in range(14):
-            sum_floor = 0
-            for l in range(j+1):
-                sum_floor += floor_1[i][l]
-            floor_1[i+1][j] = sum_floor
-    print(floor_1[k][n-1])
+    k = int(input())
+    n = int(input())
+    print(arr[k][n])
