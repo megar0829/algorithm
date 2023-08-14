7a681fa9a5ff83f4c8b4332fac9bbfdfa4d073ea
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-14 11:45:59 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 2072 ms, Memory: 58180 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+
+def dfs(n):
+    stack = [0] * N
+    top = -1
+    cnt = 1
+    visited[n] = cnt
+    while True:
+        for w in range(len(arr[n])):
+            if len(arr[n]) and visited[arr[n][w]] == 0:
+                top += 1
+                stack[top] = n
+                n = arr[n].pop(w)
+                cnt += 1
+                visited[n] = cnt
+                break
+        else:
+            if top != -1:
+                n = stack[top]
+                top -= 1
+            else:
+                return
+
+
+N, M, R = list(map(int, input().split()))
+arr = [[] for _ in range(N + 1)]
+visited = [0] * (N + 1)
+for _ in range(M):
+    v, w = map(int, input().split())
+    arr[v].append(w)
+    arr[w].append(v)
+for i in range(1, N + 1):
+    arr[i].sort(reverse=True)
+
+dfs(R)
+for i in range(1, N + 1):
+    print(visited[i])
