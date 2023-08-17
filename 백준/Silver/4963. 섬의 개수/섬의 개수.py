e7f6193e48ceeee24ffe38c299fdf60a6ff24a99
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-17 15:59:39 +0900

    [Silver II] Title: 섬의 개수, Time: 68 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,39 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+
+def dfs(x, y):
+    stack = []
+    stack.append([x, y])
+    while stack:
+        x, y = stack.pop()
+        arr[y][x] = 0
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < M and 0 <= ny < N:
+                if arr[ny][nx] == 1:
+                    stack.append([nx, ny])
+        else:
+            continue
+    return
+
+
+
+dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+dy = [0, 1, 1, 1, 0, -1, -1, -1]
+
 while True:
-    w, h = map(int, input().split())
-    if w == 0 and h == 0:
+    M, N = map(int, input().split())
+    if N == 0 and M == 0:
         break
-    cnt = 0
-    island = []
-    island.append([0 for _ in range (w+2)])
-    for i in range(1, h+1):
-        island.append(list(map(int, input().split())))
-        island[i].insert(0,0)
-        island[i].append(0) 
-    island.append([0 for _ in range (w+2)])
-    for i in range(1, h+1):
-        for j in range(1, w+1):
-            visit = []
-            if island[i][j] == 1:
-                visit.append([i,j])
-                for s in visit:
-                    a = s[0]
-                    b = s[1]
-                    island[a][b] = 2
-                    if island[a][b+1] == 1 and [a,b+1] not in visit:
-                        visit.append([a,b+1])
-                    if island[a][b-1] == 1 and [a,b-1] not in visit:
-                        visit.append([a,b-1])
-                    if island[a+1][b] == 1 and [a+1,b] not in visit:
-                        visit.append([a+1,b])
-                    if island[a+1][b+1] == 1 and [a+1,b+1] not in visit:
-                        visit.append([a+1,b+1])
-                    if island[a+1][b-1] == 1 and [a+1,b-1] not in visit:
-                        visit.append([a+1,b-1])
-                    if island[a-1][b] == 1 and [a-1,b] not in visit:
-                        visit.append([a-1,b])
-                    if island[a-1][b+1] == 1 and [a-1,b+1] not in visit:
-                        visit.append([a-1,b+1])
-                    if island[a-1][b-1] == 1 and [a-1,b-1] not in visit:
-                        visit.append([a-1,b-1])
-                cnt += 1
-    print(cnt)
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    result = 0
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 1:
+                result += 1
+                dfs(j, i)
+    print(result)
