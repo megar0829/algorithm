2ec8b8a3b0613a515c5abe739a08fc1aa087dabc
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-17 12:15:59 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 123 ms, Memory: 53,356 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -1,14 +1,18 @@
 for _ in range(10):
-    T = int(input())
-    password = list(map(int, input().split()))
-    minus = 1
-    while True:
-        password.append(password[0]-minus)
-        password.pop(0)
-        minus += 1
-        if minus == 6:
-            minus = 1
-        if password[7] <= 0:
-            password[7] = 0
-            break
-    print(f'#{T}', *password)
+    tc = int(input())
+    arr = list(map(int, input().split()))
+    check = True
+    front = -1
+    while check:
+        for i in range(5):
+            front = (front + 1) % 8
+            arr[front] -= (i + 1)
+            if arr[front] <= 0:
+                arr[front] = 0
+                check = False
+                break
+    result = [0] * 8
+    for i in range(8):
+        front = (front + 1) % 8
+        result[i] = arr[front]
+    print(f'#{tc}', *result)
