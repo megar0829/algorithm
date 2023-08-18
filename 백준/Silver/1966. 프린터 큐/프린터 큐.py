3b1842931ad99ad32ed4931fb665def6f0af4b56
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-18 09:35:50 +0900

    [Silver III] Title: 프린터 큐, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,29 +1,22 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
 T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
-    importance = deque(list(map(int, input().split())))
-    cnt = 0
+    arr = list(map(int, input().split()))
+    arr_idx = [[val, idx] for idx, val in enumerate(arr)]
+    arr_sort = sorted(arr)
+    cnt = 1
+    front = -1
+    max_idx = arr_sort.pop()
     while True:
-        print_num = importance.popleft()
-        if len(importance) == 0:
+        front = (front + 1) % len(arr_idx)
+        if arr_idx[front][0] == max_idx:
+            if arr_idx[front][1] == M:
+                print(cnt)
+                break
+            arr_idx.pop(front)
+            max_idx = arr_sort.pop()
             cnt += 1
-            break
-        else:
-            # 중요도가 높은 문서가 남아 있다면 맨 뒤로 보내기
-            if  print_num < max(importance):
-                importance.append(print_num)
-                M -= 1
-                if M < 0:
-                    M = len(importance) - 1
-            # 중요도가 높은 문서라면 출력 순서 + 1
-            elif print_num >= max(importance):
-                cnt += 1
-                if M == 0:
-                    break  
-                M -= 1
-    print(cnt)
-        
+            front -= 1
