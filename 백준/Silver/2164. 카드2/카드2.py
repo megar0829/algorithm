f008d29292d43be8cb49b07dc1cbdc0434430396
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-19 20:53:29 +0900

    [Silver IV] Title: 카드2, Time: 228 ms, Memory: 54908 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,12 +1,8 @@
 from collections import deque
+
 N = int(input())
-if N == 1:
-    print(1)
-else:
-    cards = deque(list(range(1, N+1)))
-    while True:
-        cards.popleft()
-        cards.append(cards.popleft())
-        if (len(cards)) == 1:
-            break
-    print(cards.popleft())
+arr = deque(list(range(1, N + 1)))
+while len(arr) > 1:
+    arr.popleft()
+    arr.append(arr.popleft())
+print(arr[0])
