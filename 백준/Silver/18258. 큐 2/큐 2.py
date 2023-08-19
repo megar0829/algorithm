c164bdb9d47f3afbc436fa76e9fde6e5f25b9a03
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-19 20:59:49 +0900

    [Silver IV] Title: 큐 2, Time: 1952 ms, Memory: 110368 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,34 +1,34 @@
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-
-
 N = int(input())
-lst_queue = deque([])
+arr = [0] * N
+front = rear = -1
 for _ in range(N):
-    order = list(input().split())
+    order = input().split()
     if order[0] == 'push':
-        lst_queue.append(int(order[1]))
+        rear += 1
+        arr[rear] = int(order[1])
     elif order[0] == 'pop':
-        if len(lst_queue) == 0:
+        if rear == front:
             print(-1)
         else:
-            print(lst_queue.popleft())
+            front += 1
+            print(arr[front])
     elif order[0] == 'size':
-        print(len(lst_queue))
+        print(rear - front)
     elif order[0] == 'empty':
-        if len(lst_queue) == 0:
+        if not rear - front:
             print(1)
         else:
             print(0)
     elif order[0] == 'front':
-        if len(lst_queue) == 0:
+        if rear == front:
             print(-1)
         else:
-            print(lst_queue[0])
+            print(arr[front + 1])
     elif order[0] == 'back':
-        if len(lst_queue) == 0:
+        if rear == front:
             print(-1)
         else:
-            print(lst_queue[-1])
+            print(arr[rear])
