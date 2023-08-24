f5d9228d1894eb5b6bdf4da7242589a9e40e54be
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-24 23:39:27 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,17 +1,15 @@
-import sys
-lst = [int(sys.stdin.readline()) for _ in range(9)]
-result = []
-for q in range(3):
-    for w in range(q+1, 4):
-        for e in range(w+1, 5):
-            for r in range(e+1, 6):
-                for t in range(r+1, 7):
-                    for y in range(t+1, 8):
-                        for u in range(y+1, 9):
-                            if lst[q] + lst[w] + lst[e] + lst[r] + lst[t] + lst[y] + lst[u] == 100:
-                                for i in [lst[q], lst[w], lst[e], lst[r], lst[t], lst[y], lst[u]]:
-                                    result.append(i)
-seven = []
-for j in range(7):
-    seven.append(result[j])
-print(*sorted(seven), sep='\n')
+arr = [int(input()) for _ in range(9)]
+sum_arr = sum(arr)
+check = False
+for i in range(8):
+    for j in range(i + 1, 9):
+        if arr[i] + arr[j] == sum_arr - 100:
+            arr.pop(j)
+            arr.pop(i)
+            check = True
+            break
+    if check:
+        break
+
+for i in sorted(arr):
+    print(i)
