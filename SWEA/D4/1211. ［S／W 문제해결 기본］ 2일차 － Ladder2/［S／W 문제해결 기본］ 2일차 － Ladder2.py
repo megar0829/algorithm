7e5e85f5e569f8ef16263c2c7b1468c27c0bc956
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-08-27 19:34:16 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder2, Time: 227 ms, Memory: 61,632 KB -BaekjoonHub

--- SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.py
+++ SWEA/D4/1211. ［S／W 문제해결 기본］ 2일차 － Ladder2/［S／W 문제해결 기본］ 2일차 － Ladder2.py
@@ -1,3 +1,5 @@
+# 사다리에서 오른쪽에 값이 있을 경우
+# 1 이 끝날 때까지 오른쪽으로 이동하는 함수
 def right(arr, k, x1):
     global cnt
     if 0 <= k < 100 and 0 <= x1 < 100:
@@ -13,6 +15,8 @@ def right(arr, k, x1):
         return x1
 
 
+# 사다리에서 왼쪽에 값이 있을 경우
+# 1 이 끝날 때까지 왼쪽으로 이동하는 함수
 def left(arr, k, x2):
     global cnt
     if 0 <= k < 100 and 0 <= x2 < 100:
@@ -28,6 +32,9 @@ def left(arr, k, x2):
         return x2
 
 
+# 시작점부터 탐색하며 왼쪽 오른쪽에서 1 을 만나면
+# 방향에 맞게 함수를 호출하여 탐색하며 
+# 이동 횟수를 세는 함수
 def ladder(arr, x):
     global cnt
     for j in range(1, 100):
@@ -50,16 +57,20 @@ def ladder(arr, x):
                     x = left(arr, j, x)
     return cnt
 
+
 for _ in range(10):
     tc = int(input())
     arr = [list(map(int, input().split())) for _ in range(100)]
+    
     result = 0
-    save_cnt = {}
+    min_cnt = 1000
+    min_idx = 0
     for i in range(100):
         cnt = 0
         if arr[0][i] == 1:  # 사다리의 첫 지점이 1 이면 출발
-            cnt = ladder(arr, i)  # ladder 의 결과 도착했으면 True, 도착 못하면 False
-            save_cnt[cnt] = i
+            save_cnt = ladder(arr, i)  # ladder 의 결과 도착했으면 True, 도착 못하면 False
+            if min_cnt > save_cnt:
+                min_cnt = save_cnt
+                min_idx = i
 
-    min_cnt = sorted(save_cnt.keys())[0]
-    print(f'#{tc} {save_cnt[min_cnt]}')
+    print(f'#{tc} {min_idx}')
