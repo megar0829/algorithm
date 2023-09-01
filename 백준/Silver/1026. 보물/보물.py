68637d6b0f20fce0648d0c4f63b62a1a46d9fd00
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-09-01 14:52:21 +0900

    [Silver IV] Title: 보물, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,8 @@
+N = int(input())
+A = sorted(list(map(int, input().split())))
+B = sorted(list(map(int, input().split())), reverse=True)
+
+ans = 0
+for i in range(N):
+    ans += A[i] * B[i]
+print(ans)
