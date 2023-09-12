6d56658c0b427f031238ba7d63775c6fc423b91e
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-09-12 15:02:23 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 84 ms, Memory: 32624 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,13 +1,16 @@
 import sys
 input = sys.stdin.readline
+from copy import deepcopy
 
-S = input()
+S = input().strip('\n')
 q = int(input())
+
+dp_dict = [{} for _ in range(len(S) + 1)]
+for i in range(len(S)):
+    dp_dict[i + 1] = deepcopy(dp_dict[i])
+    dp_dict[i + 1][S[i]] = dp_dict[i + 1].get(S[i], 0) + 1
+
 for _ in range(q):
     alpha, l, r = input().split()
     l, r = int(l), int(r)
-    cnt = 0
-    for i in range(l, r + 1):
-        if S[i] == alpha:
-            cnt += 1
-    print(cnt)
+    print(dp_dict[r + 1].get(alpha, 0) - dp_dict[l].get(alpha, 0))
