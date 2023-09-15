47e529125d27290b1f652b705283dad71124eba7
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-09-15 17:44:41 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,8 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-times = sorted(list(map(int, input().split())))
-min_time = [0 for _ in range(N + 1)]
-for i in range(1, N + 1):
-    min_time[i] = min_time[i - 1] + times[i - 1]
-print(sum(min_time))
+arr = sorted(list(map(int, input().split())))
+
+dp = [0] * N
+dp[0] = arr[0]
+for i in range(1, N):
+    dp[i] = dp[i - 1] + arr[i]
+print(sum(dp))
