33847cf1d358ba10f91a2c68ce328f3a308fb7cb
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-09-20 00:16:01 +0900

    [Silver II] Title: 외판원 순회 2, Time: 536 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+
+def backTracking(start, i, cost):
+    global min_val
+    if i == N - 1 and arr[start][0] != 0:
+        min_val = min(min_val, cost + arr[start][0])
+        return
+    
+    for j in range(N):
+        if not visited[j] and arr[start][j]:
+            visited[j] = 1
+            backTracking(j, i + 1, cost + arr[start][j])
+            visited[j] = 0
+
+
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visited = [0] * N
+min_val = 1e9
+
+visited[0] = 1
+backTracking(0, 0, 0)
+print(min_val)
