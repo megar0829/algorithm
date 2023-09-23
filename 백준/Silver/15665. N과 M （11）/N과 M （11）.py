180f73326aa9c9d17dc12573de7f1017d525aa40
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-09-23 20:45:46 +0900

    [Silver II] Title: N과 M (11), Time: 1336 ms, Memory: 69352 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -0,0 +1,22 @@
+def backTracking(i):
+    if i == M:
+        result.add(tuple(numbers))
+        return 
+    
+    for idx in range(N):
+        used[idx] = 1
+        numbers.append(arr[idx])
+        backTracking(i + 1)
+        used[idx] = 0
+        numbers.pop()
+
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+numbers = []
+used = [0] * N
+result = set()
+
+backTracking(0)
+for rlt in sorted(result):
+    print(*rlt)
