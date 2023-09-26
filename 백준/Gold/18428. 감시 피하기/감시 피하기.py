3bcca7f7f44848e530f20828c7dfedd0912d759f
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-09-26 11:57:33 +0900

    [Gold V] Title: 감시 피하기, Time: 100 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,50 +1,64 @@
 import sys
-input = sys.stdin.readline
 
 
-def backTracking(idx):
-    global result
-    if idx == 3:
-        if check():
-            result = 'YES'
-        return
+def backTracking(cnt):
+    global flag
 
-    if result == 'YES':
-        return
+    # 3개의 장애물을 설치했다면
+    if cnt == 3:
+        # 선생님의 위치에서 감시를 한다.
+        if bfs():
+            flag = True # 성공했다면 flag를 true로 초기화
+            return
+    else:
+        # 모든 빈공간에 장애물을 3개씩 설치해본다.
+        for x in range(n):
+            for y in range(n):
+                if graph[x][y] == "X":
+                    graph[x][y] = "O"
+                    backTracking(cnt + 1) # backTracking
+                    graph[x][y] = "X"
 
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j] == 'X':
-                arr[i][j] = 'O'
-                backTracking(idx + 1)
-                arr[i][j] = 'X'
 
+# bfs를 통해 감시
+def bfs():
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    for t in teacher:# 선생님의 위치에서
+        for k in range(4): # 상/하/좌/우 탐색
+            nx, ny = t
 
-def check():
-    for i, j in teacher:
-        for di, dj in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
-            ni, nj = i + di , j + dj
-            while 0 <= ni < N and 0 <= nj < N:
-                if arr[ni][nj] == 'O':
+            while 0 <= nx < n and 0 <= ny < n:
+                if graph[nx][ny] == "O":
                     break
-                if arr[ni][nj] == 'S':
+
+                # 학생이 보이면 실패
+                if graph[nx][ny] == "S":
                     return False
-                ni += di
-                nj += dj
+
+                nx += dx[k]
+                ny += dy[k]
+
+    # 모두 통과하면 학생이 안보이는 것으로 성공
     return True
 
 
-N = int(input())
-arr = [list(input().split()) for _ in range(N)]
+n = int(sys.stdin.readline())
+flag = False
+graph = []
 teacher = []
 
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == 'T':
-            teacher.append((i, j))
+# 반복문을 통해 복도 정보를 입력 받는다.
+for i in range(n):
+    graph.append(list(map(str, sys.stdin.readline().split())))
+    for j in range(n):
+        if graph[i][j] == "T": # 선생님이 있는 좌표를 저장
+            teacher.append([i, j])
 
-result = 'NO'
 
 backTracking(0)
 
-print(result)
+if flag:
+    print("YES")
+else:
+    print("NO")
