a84f6bdfba1a0567c5d137fde10e36cec62ad2e0
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-11-09 11:36:18 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 1244 ms, Memory: 56036 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.py
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.py
@@ -1,37 +1,32 @@
-def dfs(x, y, idx):
+def dfs(x, y, idx, word):
     global cnt
 
-    if idx == l + 1:
-        cnt += 1
+    if idx == 6:
         return
+
+    words.setdefault(word, 0)
+    words[word] += 1
 
     for dx, dy in [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]:
         nx, ny = (x + dx) % N, (y + dy) % M
-
-        if graph[nx][ny] == s[idx]:
-            dfs(nx, ny, idx + 1)
+        dfs(nx, ny, idx + 1, word + graph[nx][ny])
 
 
 N, M, K = map(int, input().split())
 
 graph = [list(input()) for _ in range(N)]
 
-god = []
+words = {}
+
+for i in range(N):
+    for j in range(M):
+        dfs(i, j, 1, graph[i][j])
+
+
 for _ in range(K):
-    S = input()
-    if S not in god:
-        god.append(S)
+    s = input()
 
-for s in god:
-    l = len(s) - 1
+    words.setdefault(s, 0)
 
-    result = 0
+    print(words[s])
 
-    for i in range(N):
-        for j in range(M):
-            if graph[i][j] == s[0]:
-                cnt = 0
-                dfs(i, j, 1)
-                result += cnt
-
-    print(result)
