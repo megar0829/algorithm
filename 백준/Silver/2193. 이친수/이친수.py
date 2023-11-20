0cfa187f0afe7772fc42d3e7f4e20acde51a6e25
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2023-11-20 09:12:53 +0900

    [Silver III] Title: 이친수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+dp = [0] * (N + 1)
+dp[1] = 1
+if N == 1:
+    print(dp[N])
+else:
+    dp[2] = 1
+    
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    print(dp[N])
