a5b4cfdc1997585a1c917f922d4774ae84e00064
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2024-03-15 11:11:53 +0900

    [Silver II] Title: 민균이의 계략, Time: 208 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+arr = [0] + list(map(int, input().split()))
+
+dp = [0] * (N + 1)
+
+for i in range(1, N + 1):
+    for j in range(i):
+        if arr[j] < arr[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+            
+print(max(dp))
