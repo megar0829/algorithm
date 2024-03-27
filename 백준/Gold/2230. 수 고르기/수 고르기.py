ad653166f2e46a83fc18def56dc8de9600418af4
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2024-03-27 13:31:50 +0900

    [Gold V] Title: 수 고르기, Time: 164 ms, Memory: 36148 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,34 +1,27 @@
 import sys
+input = sys.stdin.readline
 
-n, m = map(int, sys.stdin.readline().split())
+N, M = map(int, input().split())
+arr = sorted([int(input()) for _ in range(N)])
 
-# 수열 저장
-arr = []
-for _ in range(n):
-    arr.append(int(sys.stdin.readline()))
-# 수열 오름차순 정렬
-arr.sort()
+ans = 2e9
 
-# 투 포인터 시작 인덱스
 left, right = 0, 0
 
-# 두 수의 차이가 M 이상이면서 제일 작은 수
-result = int(2e9)
-
-# 투 포인터 탐색 시작
-while left <= right and right < n:
-
-    # 두 수의 차이가 M 미만일 경우
-    if arr[right]-arr[left] < m:
-        # 오른쪽 인덱스를 1 증가
-        right += 1
-
-    # 두 수의 차이가 M 이상일 경우
-    else:
-        # 두 수의 차이가 M 이상이면서 제일 작은 수를 비교한 뒤, 업데이트
-        result = min(result, arr[right]-arr[left])
-        # 왼쪽 인덱스를 1 증가
+while True:
+    if left > right:
+        break
+    
+    if right == N:
+        break
+    
+    gap = arr[right] - arr[left]
+    
+    if gap >= M:
+        ans = min(ans, gap)
         left += 1
 
-# 결과 출력
-print(result)
+    else:
+        right += 1
+        
+print(ans)
