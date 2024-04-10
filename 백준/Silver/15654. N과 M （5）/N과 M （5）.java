439a78f85ec725266e4345a89008fd165fc086ea
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2024-04-11 08:52:36 +0900

    [Silver III] Title: N과 M (5), Time: 1272 ms, Memory: 58396 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N, M;
+    private static int[] arr;
+
+    private static boolean[] used;
+
+    private static int[] sequence;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(input.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+
+        Set<Integer> intHashSet = new HashSet<>();
+
+        st = new StringTokenizer(input.readLine());
+
+        for (int i = 0; i < N; i++) {
+            intHashSet.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int idx = 0;
+
+        for (int num : intHashSet) {
+            arr[idx] = num;
+            idx++;
+        }
+
+        Arrays.sort(arr);
+
+        used = new boolean[N];
+
+        sequence = new int[N];
+
+        dfs(0);
+    }
+
+    static void dfs(int idx) {
+        if (idx == M) {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < M; i++) {
+                if (i != 0) {
+                    sb.append(' ');
+                }
+                sb.append(sequence[i]);
+            }
+            System.out.println(sb);
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                sequence[idx] = arr[i];
+                dfs(idx + 1);
+                used[i] = false;
+                sequence[idx] = 0;
+            }
+        }
+    }
+}
