36aba7250fba09784f7c4f396bdbb6463674444d
Jeongsik Bae <67329337+megar0829@users.noreply.github.com>
2024-05-16 14:58:11 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,16 @@
+classDict = {
+    "Algorithm":	"204",
+    "DataAnalysis":	"207",
+    "ArtificialIntelligence":	"302",
+    "CyberSecurity":	"B101",
+    "Network":	"303",
+    "Startup":	"501",
+    "TestStrategy":	"105"
+}
+
+N = int(input())
+
+for i in range(N):
+    name = input()
+    
+    print(classDict[name])
